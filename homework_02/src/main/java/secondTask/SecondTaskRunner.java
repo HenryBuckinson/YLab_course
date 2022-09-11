6fdfe4306b6b05ec7f486c688a8e249dafd0db08
HenryBuckinson <abdula2341@gmail.com>
2022-09-11 11:08:24 +0300

    homework_02, Task №2: 1) Убрал выброс исключений. 2) Теперь, в случае, если входные данные некорректны, или нет подходящей пары чисел в качестве результата возвращается пустой массив.

--- homework_02/src/main/java/secondTask/SecondTaskRunner.java
+++ homework_02/src/main/java/secondTask/SecondTaskRunner.java
@@ -1,59 +1,52 @@
 package secondTask;
 
-import secondTask.Exceptions.ArrayDataException;
-import secondTask.Exceptions.ArraySizeException;
-
 import java.util.Arrays;
+import java.util.Objects;
 
 public class SecondTaskRunner {
     public static void main(String[] args) {
         int[] targetArray = {3, 4, 2, 7};
-        try {
-            int[] ints2 = coupleOfSumNumber(targetArray, 10);
-            printArrayResult(ints2);
-        } catch (ArrayDataException | ArraySizeException | ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
+        int[] ints2 = coupleOfSumNumber(targetArray, 10);
+
     }
 
     private static void printArrayResult(int[] ints) {
         System.out.print("Подходящая пара чисел: ");
         Arrays.stream(ints).forEach(k -> System.out.print(" [ " + k + " ] "));
+        System.out.println();
     }
 
     /**
      * @param targetArray     интересующий массив.
      * @param resultSumNumber результат суммы первой пары чисел.
      * @return Результатом метод возвращает массив, содержащий первую пару чисел исходного массива, которые в сумме дают resultSumNumber.
-     * @throws ArrayDataException  На входной массив отсутствует ссылка.
-     * @throws ArraySizeException  Входной массив пустой.
-     * @throws ArithmeticException Подходящей пары чисел не обнаружено.
      */
-    public static int[] coupleOfSumNumber(int[] targetArray, int resultSumNumber) throws ArrayDataException, ArraySizeException, ArithmeticException {
-        if (targetArray == null) {
-            throw new ArrayDataException("На входной массив отсутствует ссылка");
-        }
-        if (targetArray.length == 0) {
-            throw new ArraySizeException("Входной массив пустой");
+    public static int[] coupleOfSumNumber(int[] targetArray, int resultSumNumber) {
+        if (targetArray == null || Objects.requireNonNull(targetArray).length == 0) {
+            System.out.println("Входной массив пустой или на него отсутствует ссылка");
+            return new int[]{};
         }
         for (int i = 0, j = targetArray.length - 1; ; ) {
-
             if (targetArray[i] + targetArray[j] < resultSumNumber) {
                 j--;
                 if (j == 0) {
-                    throw new ArithmeticException("Интересующей пары чисел не обнаружено");
+                    System.out.println("Подходящей пары чисел не обнаружено");
+                    return new int[]{};
                 }
                 continue;
             }
             if (targetArray[i] + targetArray[j] > resultSumNumber) {
                 i++;
                 if (i == targetArray.length - 1) {
-                    throw new ArithmeticException("Интересующей пары чисел не обнаружено");
+                    System.out.println("Подходящей пары чисел не обнаружено");
+                    return new int[]{};
                 }
                 continue;
             }
             if (targetArray[i] + targetArray[j] == resultSumNumber) {
-                return new int[]{targetArray[i], targetArray[j]};
+                int[] result = new int[]{targetArray[i], targetArray[j]};
+                printArrayResult(result);
+                return result;
             }
         }
     }
--- homework_02/src/test/java/secondTask/SecondTaskRunnerTest.java
+++ homework_02/src/test/java/secondTask/SecondTaskRunnerTest.java
@@ -3,8 +3,6 @@ package secondTask;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import secondTask.Exceptions.ArrayDataException;
-import secondTask.Exceptions.ArraySizeException;
 
 
 class SecondTaskRunnerTest {
@@ -16,35 +14,9 @@ class SecondTaskRunnerTest {
         Assertions.assertArrayEquals(new int[]{2, 7}, SecondTaskRunner.coupleOfSumNumber(new int[]{3, 4, 2, 7}, 9));
         Assertions.assertArrayEquals(new int[]{2, 8}, SecondTaskRunner.coupleOfSumNumber(new int[]{2, 4, 6, 8}, 10));
         Assertions.assertArrayEquals(new int[]{4, 2}, SecondTaskRunner.coupleOfSumNumber(new int[]{8, 6, 4, 2}, 6));
+        Assertions.assertArrayEquals(new int[]{}, SecondTaskRunner.coupleOfSumNumber(new int[]{3, 4, 2, 7}, 12));
+        Assertions.assertArrayEquals(new int[]{}, SecondTaskRunner.coupleOfSumNumber(null, 12));
+        Assertions.assertArrayEquals(new int[]{}, SecondTaskRunner.coupleOfSumNumber(new int[]{}, 12));
     }
 
-    @Test
-    @DisplayName("Обработка исключения в случае, если не удается найти подходящую пару чисел")
-    void shouldThrowArithmeticException1() {
-        try {
-            SecondTaskRunner.coupleOfSumNumber(new int[]{3, 4, 2, 7}, 12);
-        } catch (ArithmeticException e) {
-            Assertions.assertEquals("Интересующей пары чисел не обнаружено", e.getMessage());
-        }
-    }
-
-    @Test
-    @DisplayName("Обработка исключения в случае, если входной массив пустой")
-    void shouldThrowArithmeticException2() {
-        try {
-            SecondTaskRunner.coupleOfSumNumber(new int[]{}, 12);
-        } catch (ArraySizeException e) {
-            Assertions.assertEquals("Входной массив пустой", e.getMessage());
-        }
-    }
-
-    @Test
-    @DisplayName("Обработка исключения в случае, если на входной массив отсутствует ссылка")
-    void shouldThrowArithmeticException3() {
-        try {
-            SecondTaskRunner.coupleOfSumNumber(null, 12);
-        } catch (ArrayDataException e) {
-            Assertions.assertEquals("На входной массив отсутствует ссылка", e.getMessage());
-        }
-    }
 }
